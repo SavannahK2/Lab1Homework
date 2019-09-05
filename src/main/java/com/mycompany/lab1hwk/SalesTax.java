e3cbeb80f21f3b3857ef8e7630fbf87aebf55023
0786954 <0786954@KELCE-LABS11-P.pittstate.edu>
2019-09-05 10:59:27 -0500

    First Commit

---
+++ src/main/java/com/mycompany/lab1hwk/SalesTax.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lab1hwk;
+
+/**
+ *
+ * @author 0786954
+ */
+public class SalesTax {
+    public static void main(String[] args) {
+        
+    }
+}
