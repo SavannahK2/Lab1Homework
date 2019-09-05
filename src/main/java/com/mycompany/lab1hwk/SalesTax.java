da414451cb32a9be79524637dcf2119a9f2c872e
0786954 <0786954@KELCE-LABS11-P.pittstate.edu>
2019-09-05 11:52:34 -0500

    Second commit. Few errors. User Error catch not working Ending calculation showing too many numbers

--- src/main/java/com/mycompany/lab1hwk/SalesTax.java
+++ src/main/java/com/mycompany/lab1hwk/SalesTax.java
@@ -5,12 +5,48 @@
  */
 package com.mycompany.lab1hwk;
 
-/**
- *
- * @author 0786954
- */
+import java.util.Scanner;
 public class SalesTax {
     public static void main(String[] args) {
-        
+        // Constants
+      final double StateTaxRate = 0.04;  // State tax rate
+      final double CountyTaxRate = 0.02; // County tax rate
+      
+      // Variables
+      double purchase;         // Amount of purchase
+      double stateTax;         // Amount of state sales tax
+      double countyTax;        // Amount of county sales tax
+      double totalTax;         // Total sales tax
+      double totalCost;        // Total cost of the purchase
+      
+      // Create a Scanner object for keyboard input.
+      Scanner keyboard = new Scanner(System.in);
+             
+      // Get the amount of the purchase.
+      System.out.print("Enter the purchase amount here please: ");
+      purchase = keyboard.nextDouble();
+      String purchaseNumberAsString = keyboard.nextLine();
+    
+      // Catch user error: STILL INCORRECT
+      try {
+          float purchaseNumberAsFloat = Float.parseFloat(purchaseNumberAsString);
+          System.out.print("Thank you.");
+      } catch (NumberFormatException x) {
+          System.out.print("Type only numbers and a decimal for cents (only two decimal places for cents allowed)");
+      }
+     
+      
+      // Calculations
+      stateTax = purchase * StateTaxRate;
+      countyTax = purchase * CountyTaxRate;
+      totalTax = stateTax + countyTax;
+      totalCost = purchase + totalTax;
+      
+      // Display the results.
+      System.out.println("Purchase amount: $" + purchase);
+      System.out.println("State tax: $" + stateTax);
+      System.out.println("County tax: $" + countyTax);
+      System.out.println("Total tax: $" + totalTax);
+      System.out.println("Total cost: $" + totalCost);
     }
 }
